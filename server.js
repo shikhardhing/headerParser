3ef93dcc3af31492f713d45b329378b0f0fe4711
Shikhar Dhing <shikhardhing@gmail.com>
2016-08-08 16:37:41 +0530

    port

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.get('/', function (req, res) {
 	console.log(req.ip+"\n");
 	res.send('{"ipaddress":"'+req.ip+'","language":"'+req.acceptsLanguages()[0]+'","software":"'+o.type()+" "+o.arch()+'"}');
 });
+app.get('/*',function(req,res){
+	req.send("Hello");
+})
 var server = app.listen(app.get('port'), function () {
 	var host = 'localhost';
 	var port = server.address().port;
 	console.log("Listening at http://%s:%s", host, port);
+  console.log('Node app is running on port', app.get('port'));
 });
