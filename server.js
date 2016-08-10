d60161793005a346d77637ed5817de646195b35a
Shikhar Dhing <shikhardhing@gmail.com>
2016-08-10 13:10:22 +0530

    a

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ var express = require('express');
 var o=require('os');
 var proces=require('process');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
 	console.log(req.ip+"\n");
 	res.send('{"ipaddress":"'+req.ip+'","language":"'+req.acceptsLanguages()[0]+'","software":"'+o.type()+" "+o.arch()+'"}');
 });
 app.get('/*',function(req,res){
-	req.send("Hello");
+	res.send("Hello");
 })
 var server = app.listen(process.env.PORT || 5000, function () {
 	var host = 'localhost';
 	var port = server.address().port;
 	console.log("Listening at http://%s:%s", host, port);
-  console.log('Node app is running on port', app.get('port'));
 });
